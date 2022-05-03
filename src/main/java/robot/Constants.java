5e1af7480ce4a26e61318f5ddeab64580c41036e
FrenchWarLord <dyllruggs@gmail.com>
2022-05-03 15:40:07 -0700

    test commit. ignore

--- src/main/java/robot/Constants.java
+++ src/main/java/robot/Constants.java
@@ -27,7 +27,6 @@ public class Constants {
     public static double shooter_speed_fire = 0.92;  // shooting-specific
     // public static double shooter_speed_fire = 1.0;  // shooting-specific (West Valley)
 
-    //test
 
     // Conveyor motor constants
     public static double conveyor_speed = 0.40;
